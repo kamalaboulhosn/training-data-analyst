41b2464a562b8d1d2699e4a6acc01ca3bb083d90
Kamal Aboul-Hosn <kamal.aboulhosn@gmail.com>
2019-11-12 15:53:31 +0000

    Update run script

--- courses/data_analysis/deepdive/pubsub-exercises/exercise1/run.sh
+++ courses/data_analysis/deepdive/pubsub-exercises/exercise1/run.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 set -m
 PROJECT=`curl http://metadata.google.internal/computeMetadata/v1/project/project-id -H "Metadata-Flavor: Google"`
-SUBSCRIPTION_KEY=`date +"%H%M%S%N"`
-SUBSCRIPTION=pubsub-e2e-example-sub-$SUBSCRIPTION_KEY
-OLD_SUBSCRIPTION=`cat .last_subscription`
-echo "Delete old subscription."
-CLOUDSDK_API_ENDPOINT_OVERRIDES_PUBSUB=https://loadtest-pubsub.sandbox.googleapis.com/ gcloud pubsub subscriptions delete $OLD_SUBSCRIPTION
-echo "Create new subscription."
-CLOUDSDK_API_ENDPOINT_OVERRIDES_PUBSUB=https://loadtest-pubsub.sandbox.googleapis.com/ gcloud pubsub subscriptions create --topic pubsub-e2e-example $SUBSCRIPTION
-echo $SUBSCRIPTION > .last_subscription
+# Manually create the subscription ordered-subscription-test as a subscription on pubsub-e2e-example
+SUBSCRIPTION=ordered-subscription-test
 echo "Starting publisher and publishing 1M messages."
 java -Xmx16384m -cp target/pubsub.jar com.google.cloud.sme.pubsub.Publisher -p $PROJECT
 echo "Starting subscriber."
